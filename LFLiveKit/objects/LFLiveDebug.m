4e5e884ecf03f0f63a765261361699fa48c2c725
Nam Kennic <namkennic@me.com>
2020-06-11 14:57:44 +0700

    no message

--- LFLiveKit/objects/LFLiveDebug.m
+++ LFLiveKit/objects/LFLiveDebug.m
@@ -11,7 +11,7 @@
 @implementation LFLiveDebug
 
 - (NSString *)description {
-    return [NSString stringWithFormat:@"丢掉的帧数:%ld 总帧数:%ld 上次的音频捕获个数:%d 上次的视频捕获个数:%d 未发送个数:%ld 总流量:%0.f",_dropFrame,_totalFrame,_currentCapturedAudioCount,_currentCapturedVideoCount,_unSendCount,_dataFlow];
+	return [NSString stringWithFormat:@"DropFrame:%ld TotalFrames:%ld AudioCount:%d VideoCount:%d UnsendCount:%ld TotalFlow:%0.f",(long)_dropFrame,(long)_totalFrame,_currentCapturedAudioCount,_currentCapturedVideoCount,(long)_unSendCount,_dataFlow];
 }
 
 
